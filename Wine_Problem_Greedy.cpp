93d4994bf4889cab17fc6ed173322eb89e3b5772
Aryan Omar <35742681+Aryanomar00@users.noreply.github.com>
2019-07-03 00:22:53 +0530

    Add files via upload

---
+++ Wine_Problem_Greedy.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int cnt=0;
+int maxprofit(int arr[],int st,int en,int year)
+{
+    cnt++;
+    if(st>en)
+    {
+        return 0;
+    }
+    int q1=arr[st]*year+maxprofit(arr,st+1,en,year+1);
+    int q2=arr[en]*year+maxprofit(arr,st,en-1,year+1);
+    int ans=max(q1,q2);
+    return ans;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int st=0;
+    int en=n-1;
+    int ans=maxprofit(arr,0,n-1,1);
+    cout<<ans<<endl<<cnt;
+
+}
